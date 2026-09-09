fcfd793825837afa46d531bfb9dba5965b7100c2
Chaman Vashishth <chamanvashishth133@gmail.com>
2026-09-09 22:16:41 +0530

    Update Day 19 solution for n up to 10^15

--- Day-19/Total-Number-of-Commas.cpp
+++ Day-19/Total-Number-of-Commas.cpp
@@ -1,10 +1,21 @@
 class Solution {
 public:
-    long long countCommas(int n) {
-        if (n < 1000) {
-            return 0;
+    long long countCommas(long long n) {
+        long long total = 0;
+        long long start = 1000;
+        long long commas = 1;
+
+        while (start <= n) {
+            long long end = start * 1000 - 1;
+            long long last = min(n, end);
+            long long count = last - start + 1;
+
+            total += count * commas;
+
+            start *= 1000;
+            commas++;
         }
 
-        return n - 999;
+        return total;
     }
 };
